64f0ec4b7b3b6416ac0e10cef4c66edfa0332439
Cardonos <119761878+Cardonos@users.noreply.github.com>
2023-12-01 11:38:22 +0100

    1st

--- 1.12.py
+++ 1.12.py
@@ -1,5 +1,30 @@
 import numpy as np
 
-input = np.loadtxt("test.txt")
+input = np.loadtxt("Inputs/1.12.txt", dtype='str')
 
-print(input)
+output = 0
+
+for i in input:
+    digOne = 0
+    digTwo = 0
+    j = 0
+    while j <= (len(i)+1):
+        try:
+            digOne = digOne + int(i[j])
+            break
+        except:
+            j += 1
+    j = 0
+    while j <= (len(i)+1):
+        try:
+            digTwo = digTwo + int(i[(len(i)+1)-j])
+            break
+        except:
+            j += 1
+    fullNum = int(str(digOne) + str(digTwo))
+    print(digOne)
+    print(digTwo)
+    print(fullNum)
+    output = output + fullNum
+
+print(output)
