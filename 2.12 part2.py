52d08ca272ceaa081e36e65ab6e2ebf65a4bc5f0
Cardonos <119761878+Cardonos@users.noreply.github.com>
2023-12-02 10:20:34 +0100

    2.12 part1+2, more readable

--- 2.12 part2.py
+++ 2.12 part2.py
@@ -7,33 +7,30 @@ for i in data:
 
 parsedData = tempData
 tempData = []
-
 for i in parsedData:
     for j in i:
         tempData.append(j.split("; "))
-
 parsedData = tempData
-tempData = []
-
 sumOfPowers = 0
 k = 0
-while k <= len(parsedData)-2:
+while k <= len(parsedData)-2:  # iterating over all games
     minRed = 0
     minGreen = 0
     minBlue = 0
     l = 0
-    while l < len(parsedData[k+1]):
+    while l < len(parsedData[k+1]):  # iterating over all sets of one game
         m = 0
-        for i in parsedData[k+1][l].split(", "):
-            if i[-1] == "d":
-                if int(parsedData[k+1][l].split(", ")[m][0:2]) > minRed:
-                    minRed = int(parsedData[k+1][l].split(", ")[m][0:2])
+        for i in parsedData[k+1][l].split(", "):  # iterating over all cubes of one set  of one game
+            numberOfCubes = int(parsedData[k+1][l].split(", ")[m][0:2])
+            if i[-1] == "d":                    # checks if the current looked at number of cubes is higher than the
+                if numberOfCubes > minRed:      # previously needed minimum number for that game and updates the
+                    minRed = numberOfCubes      # minimum accordingly
             if i[-1] == "n":
-                if int(parsedData[k+1][l].split(", ")[m][0:2]) > minGreen:
-                    minGreen = int(parsedData[k+1][l].split(", ")[m][0:2])
+                if numberOfCubes > minGreen:
+                    minGreen = numberOfCubes
             if i[-1] == "e":
-                if int(parsedData[k+1][l].split(", ")[m][0:2]) > minBlue:
-                    minBlue = int(parsedData[k+1][l].split(", ")[m][0:2])
+                if numberOfCubes > minBlue:
+                    minBlue = numberOfCubes
             m += 1
         l += 1
     powerOfGame = minRed*minBlue*minGreen
--- 2.12.py
+++ 2.12.py
@@ -7,38 +7,38 @@ for i in data:
 
 parsedData = tempData
 tempData = []
-
 for i in parsedData:
     for j in i:
         tempData.append(j.split("; "))
 
 parsedData = tempData
-tempData = []
-
 numberRed = 12
 numberGreen = 13
 numberBlue = 14
 sumOfPossible = 0
 
 k = 0
-while k <= len(parsedData)-2:
+while k <= len(parsedData)-2:  # iterating over all games
     l = 0
     gamePossible = True
-    while l < len(parsedData[k+1]):
+    while l < len(parsedData[k+1]) and gamePossible:  # iterating over all sets of one game as long as its possible
         m = 0
-        for i in parsedData[k+1][l].split(", "):
-            if i[-1] == "d":
-                if int(parsedData[k+1][l].split(", ")[m][0:2]) > numberRed:
+        for i in parsedData[k+1][l].split(", "):  # iterating over all cubes of one set of one game
+            numberOfCubes = int(parsedData[k+1][l].split(", ")[m][0:2])  # reads out the number of cubes of the current
+            if i[-1] == "d":  # checks if the color is red               # looked at color
+                if numberOfCubes > numberRed:
                     gamePossible = False
-            if i[-1] == "n":
-                if int(parsedData[k+1][l].split(", ")[m][0:2]) > numberGreen:
+                    break
+            if i[-1] == "n":  # checks if the color is green
+                if numberOfCubes > numberGreen:
                     gamePossible = False
-            if i[-1] == "e":
-                if int(parsedData[k+1][l].split(", ")[m][0:2]) > numberBlue:
+                    break
+            if i[-1] == "e":  # checks if the color is blue
+                if numberOfCubes > numberBlue:
                     gamePossible = False
             m += 1
         l += 1
     if gamePossible:
-        sumOfPossible = sumOfPossible + int(parsedData[k][-1][5:])
+        sumOfPossible = sumOfPossible + int(parsedData[k][-1][5:])  # adds the current game id onto the sum
     k += 2
 print(sumOfPossible)
